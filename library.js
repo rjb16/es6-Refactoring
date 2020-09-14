27fd70b3f98754245f5a05b0b0f1d8c6bcd14df9
Ruben Botha <rubenjohannesbotha@gmail.com>
2020-09-14 08:52:47 +0100

    finished

---
+++ library.js
@@ -0,0 +1,27 @@
+class Library{
+  constructor(books = []) {
+    this.books = books;
+  }
+
+  bookCount() {
+    return this.books.length;
+  }
+
+  addBook(newBook) {
+    this.books.push(newBook);
+  }
+
+  addBooks(newBooks) {
+    newBooks.forEach(book => this.books.push(book));
+  }
+
+  printInventory() {
+    this.books.forEach((book) => {
+      const {author, title} = book;
+      console.log(`${title} by ${author}`);
+    });
+  }
+
+}
+
+export {Library};
